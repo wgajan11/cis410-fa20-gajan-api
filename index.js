8cd3d38d9b74f644680e99b4d7c3a726b8fdeaa7
wgajan11 <westeast@rams.colostate.edu>
2020-11-08 18:57:10 -0700

    10.1 10/27 /products/:pk

--- index.js
+++ index.js
@@ -16,8 +16,38 @@ app.get("/products", (req,res)=>{
     .then((result)=>{
         res.status(200).send(result)
     })
-    .catch((err)=>{console.log(err);
-    res.status(500).send()})
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
     
 })
+
+app.get("/products/:ProductID", (req,res)=>{
+    var pk = req.params.ProductID
+    // console.log(pk)
+
+    var myQuery = `SELECT *
+    FROM product
+    left join department
+    on department.DepartmentID = Product.DepartmentFK
+    where ProductID = ${pk}`
+
+    db.executeQuery(myQuery)
+        .then((products)=>{
+            // console.log(products)
+
+            if(products[0]){
+                res.send(products[0])
+
+            }
+            else{res.status(404).send("bad requeset")}
+        })
+        .catch((err)=>{
+            console.log("error in /products/pk", err)
+            res.status(500).send()
+        })
+})
+
+
 app.listen(5000, ()=>{console.log("app is running on port 5000")})
